f703e39e1546193aa1395ee17de6d8833f72baff
Robert Adovor Kakah <robertadovor01@gmail.com>
2025-09-18 01:15:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -- coding: utf-8 --
+"""
+A web applicaton which allows users to view the weather
+"""
 import streamlit as st
 import requests
 import json
@@ -269,4 +274,5 @@ def main():
     """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
+
     main()
